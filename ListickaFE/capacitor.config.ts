a148ce833957bdc9565239c448314d4ea7185e40
Merel Ohler <merel.ohler@gmail.com>
2025-01-23 17:30:48 -0700

    added angular

---
+++ ListickaFE/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'ListickaFE',
+  webDir: 'www'
+};
+
+export default config;
